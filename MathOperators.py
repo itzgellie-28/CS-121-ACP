2fc83ae251e555db4af11026f3310af8e0bbcf62
itzgellie-28 <24-42704@g.batstate-u.edu.ph>
2025-11-21 08:18:10 +0800

    MathOperators

---
+++ MathOperators.py
@@ -0,0 +1,10 @@
+x = int(input("Enter first number: "))
+y = int(input("Enter second number: "))
+
+print("Numbers entered:", x, y)
+difference = x - y
+print("The difference is:", difference)
+
+a = True
+b = False
+print("Is x greater than y?", x > y)
